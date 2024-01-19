4be9abd27e83481acebbece414a05f9e1b25216e
SuhasGD007 <141063784+SuhasGD007@users.noreply.github.com>
2024-01-19 11:47:47 +0530

    updated

--- src/main/java/com/example/demo/controller/paymentController.java
+++ src/main/java/com/example/demo/controller/paymentController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.entities.Users;
@@ -12,6 +13,7 @@ import com.example.demo.services.UsersService;
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.razorpay.Utils;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -22,16 +24,28 @@ public class paymentController {
 
 	@GetMapping("/pay")
 	public String pay() {
+		
 		return "pay";
 	}
+	
+	@GetMapping("/payment-success")
+	public String paymentSuccess(HttpSession session) {
+		String mail =  (String) session.getAttribute("email");
+		Users u = service.getUsers(mail);
+		u.setPremium(true);
+		service.updateUser(u);
+		return "customerHome";
+	}
+	
+	@GetMapping("/payment-failure")
+	public String paymentFailure() {
+		return "customerHome";
+	}
 
-	
-	//github.com/deep473/tuneHub (gitHub link for code)
-	
 	@SuppressWarnings("finally")
 	@PostMapping("/createOrder")
 	@ResponseBody
-	public String createOrder(HttpSession session) {
+	public String createOrder() {
 
 		int  amount  = 5000;
 		Order order=null;
@@ -45,11 +59,7 @@ public class paymentController {
 
 			order = razorpay.orders.create(orderRequest);
 
-			String mail =  (String) session.getAttribute("email");
-
-			Users u = service.getUsers(mail);
-			u.setPremium(true);
-			service.updateUser(u);
+			
 
 		} catch (RazorpayException e) {
 			e.printStackTrace();
@@ -58,4 +68,23 @@ public class paymentController {
 			return order.toString();
 		}
 	}	
+	
+	@PostMapping("/verify")
+	@ResponseBody
+	public boolean verifyPayment(@RequestParam  String orderId, @RequestParam String paymentId, @RequestParam String signature) {
+	    try {
+	        // Initialize Razorpay client with your API key and secret
+	        RazorpayClient razorpayClient = new RazorpayClient("rzp_test_sB1NPCKKI3GpoX", "k75wgHglUtduE4P7KiCn0Shf");
+	        // Create a signature verification data string
+	        String verificationData = orderId + "|" + paymentId;
+
+	        // Use Razorpay's utility function to verify the signature
+	        boolean isValidSignature = Utils.verifySignature(verificationData, signature, "k75wgHglUtduE4P7KiCn0Shf");
+
+	        return isValidSignature;
+	    } catch (RazorpayException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
 }
